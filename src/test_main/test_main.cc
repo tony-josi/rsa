f8b865f2206ccb9088e2e7c14fb4241c2a2602dc
TonyJosi97 <tonyjosinew@gmail.com>
2021-11-17 23:42:26 +0530

    test exe main()

--- src/test_main/test_main.cc
+++ src/test_main/test_main.cc
@@ -8,8 +8,8 @@ int main () {
     bi::big_int pub_key = rsa_128.get_public_key();
     bi::big_int priv_key = rsa_128.get_private_key();
 
-    std::cout<<"PUB: "<<pub_key.big_int_to_string();
-    std::cout<<"PRIV: "<<priv_key.big_int_to_string();
+    std::cout<<"PUB: "<<pub_key.big_int_to_string()<<"\n";
+    std::cout<<"PRIV: "<<priv_key.big_int_to_string()<<"\n";
 
     return 0;
 
