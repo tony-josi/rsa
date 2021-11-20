a554b70b5e5d72d9ebcac0fd301a9bdc455987b1
TonyJosi97 <tonyjosinew@gmail.com>
2021-11-21 00:12:15 +0530

    fixed key naming issue

--- src/rsa/rsa.cc
+++ src/rsa/rsa.cc
@@ -30,13 +30,13 @@ rsa::rsa(size_t bit_size_arg) {
     ret_val += q.big_int_unsigned_sub(bi_1, q_minus_1);
     ret_val += p_minus_1.big_int_multiply(q_minus_1, p_minus_1q_minus_1);
 
-    ret_val += d.big_int_from_base_type(DEFAULT_32_BIT_PUBLIC_KEY, false);
+    ret_val += e.big_int_from_base_type(DEFAULT_32_BIT_PUBLIC_KEY, false);
 
-    if (d.big_int_unsigned_compare(p_minus_1q_minus_1) >= 0) {
+    if (e.big_int_unsigned_compare(p_minus_1q_minus_1) >= 0) {
         throw std::invalid_argument("Error initializing RSA");
     }
 
-    ret_val += d.big_int_modular_inverse_extended_euclidean_algorithm(p_minus_1q_minus_1, e);
+    ret_val += e.big_int_modular_inverse_extended_euclidean_algorithm(p_minus_1q_minus_1, d);
 
     if (ret_val != 0) {
         throw std::invalid_argument("Error initializing RSA");
@@ -45,11 +45,11 @@ rsa::rsa(size_t bit_size_arg) {
 }
 
 bi::big_int rsa::get_private_key() {
-    return e;
+    return d;
 }
 
 bi::big_int rsa::get_public_key() {
-    return d;
+    return e;
 }
 
 bi::big_int rsa::get_modulus() {
@@ -62,7 +62,7 @@ int rsa::rsa_encrypt(bi::big_int &plain, bi::big_int &cipher) {
         throw std::invalid_argument("Plain text too long");
     }
 
-    return plain.big_int_fast_modular_exponentiation(d, pq, cipher);
+    return plain.big_int_fast_modular_exponentiation(e, pq, cipher);
 }
 
 int rsa::rsa_decrypt_1(bi::big_int &cipher, bi::big_int &decipher) {
@@ -71,7 +71,7 @@ int rsa::rsa_decrypt_1(bi::big_int &cipher, bi::big_int &decipher) {
         throw std::invalid_argument("Cipher text too long");
     }
 
-    return cipher.big_int_fast_modular_exponentiation(e, pq, decipher);
+    return cipher.big_int_fast_modular_exponentiation(d, pq, decipher);
 }
 
 int rsa::rsa_decrypt(bi::big_int &cipher, bi::big_int &decipher) {
@@ -84,7 +84,7 @@ int rsa::rsa_decrypt(bi::big_int &cipher, bi::big_int &decipher) {
     bi::big_int cipher_reduced_p;
     ret_val += cipher.big_int_modulus(p, cipher_reduced_p);
     bi::big_int reduced_power;
-    ret_val += e.big_int_modulus(p_minus_1, reduced_power);
+    ret_val += d.big_int_modulus(p_minus_1, reduced_power);
     return ret_val + cipher_reduced_p.big_int_fast_modular_exponentiation(reduced_power, p, decipher);
 
 }
